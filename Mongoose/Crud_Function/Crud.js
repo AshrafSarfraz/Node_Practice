8735fb9e471d53dea011bc35478a5d9b73f5a3ca
AshrafSarfraz <ashrafq952@gmail.com>
2024-02-24 11:15:55 +0500

    Mongoose

--- Mongoose/Crud_Function/Crud.js
+++ Mongoose/Crud_Function/Crud.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
-
-const Productscheme= new mongoose.Schema({
+mongoose.connect('mongodb://127.0.0.1:27017/e-comm');   // connect MongoDB  with the help of Mongoose
+ 
+const Productscheme= new mongoose.Schema({     
    name:String,
    age:Number,
 })
